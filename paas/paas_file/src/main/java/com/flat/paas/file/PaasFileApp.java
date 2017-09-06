d75f102b0d7ece0f268332d01bc1ef4f3400c125
kangjin.zhao <270882007@qq.com>
2017-09-06 21:15:46 +0800

    文件模块首次提交

---
+++ paas/paas_file/src/main/java/com/flat/paas/file/PaasFileApp.java
@@ -0,0 +1,21 @@
+package com.flat.paas.file;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * @ClassName: PaasFileApp   
+ * @Description: TODO  
+ * @author: kangjin.zhao  
+ * @date:2017年9月6日 下午9:12:58
+ */
+public class PaasFileApp {
+	private final static Logger log = LoggerFactory.getLogger(PaasFileApp.class);
+	/**   
+	 * @Title: main   
+	 * @Description: TODO  格式要求，无实际作用
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		log.info("PaaS通用包已加载...");
+	}
+}
